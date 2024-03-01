c9f62441f48acfcbcaec7df8e8eb8bc2ef70d04d
Hossam-Osama <osamahossam899@gmail.com>
2024-03-01 17:28:18 +0200

    finish

---
+++ js/big.js
@@ -0,0 +1,6 @@
+
+document.getElementsByClassName("icons").onclick="myFunction()";
+document.getElementById("demo").style.display = 'none';
+function myFunction() {
+    document.getElementById("demo").style.display = 'block';
+}
